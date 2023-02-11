831c07ee2c38d40a26efa1de8c0fa19238735159
sangitasarkar74 <sangitasarkar@gmail.com>
2023-02-11 23:03:50 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,32 @@
-import React , {useState,useEffect} from 'react'
-import '../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
 import List from "./List";
 const App = () => {
   //code here
+  const [name, setname] = useState("");
+  const [list, setlist] = useState([]);
+
+  const handleChange = (e) => {
+    setname(e.target.value);
+  };
+  const handleOnClick = () => {
+    // setlist([...name, list])
+    // list.push(name)
+    setname("");
+    setlist([...list, name]);
+  };
+
   return (
     <div id="main">
-       <input id="input" onChange={} />
-      <button id="button" onClick={}>Click</button>
-       <ul id="list">
-      <List listx={list} />
+      <input id="input" onChange={handleChange} />
+      <button id="button" value={name} onClick={handleOnClick}>
+        Click
+      </button>
+      <ul id="list">
+        <List listx={list} />
       </ul>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ListItems from './ListItems';
+import React from "react";
+import ListItems from "./ListItems";
 //code here
- const List = ({listx}) =>{
-  return(
-    
- 
-    <ListItems valuex={i} />
-    
-  
-  )
-}
+const List = ({ listx }) => {
+  return (
+    <>
+      <ul id="list">
+        {listx.map((list) => (
+          <ListItems valuex={list} />
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default List;
